16353be46e7031bb94207e6b6354c8c414318376
haihong ren <haihong.ren@gmail.com>
2018-07-15 15:56:07 +1000

    added deck type and function

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+	// bestP := bestPlayer(-10, 55, 10, 501, 501, 505, 3083)
+	cards := deck{"a", "b"}
+	cards.print()
+	// for a, c := range cards {
+	// 	fmt.Println(a, c)
+	// }
+
+	// fmt.Println(cards)
+	// _ = bestP
+
+}
+
+func newCard() string {
+	return "ace of heart"
+
+}
--- test2.go
+++ test2.go
@@ -6,12 +6,23 @@ import (
 
 func main() {
 
-	bestP := bestPlayer(-10, 55, 10, 501, 501, 505, 3083)
+	// bestP := bestPlayer(-10, 55, 10, 501, 501, 505, 3083)
+	cards := []string{newCard(), "aces of dimonds", newCard()}
+	cards = append(cards, "six of dimond")
 
-	fmt.Println(bestP)
+	for a, c := range cards {
+		fmt.Println(a, c)
+	}
+
+	// fmt.Println(cards)
+	// _ = bestP
 
 }
 
+func newCard() string {
+	return "ace of heart"
+
+}
 func bestPlayer(players ...int) int {
 	bestS := players[0]
 	for a, s := range players {
